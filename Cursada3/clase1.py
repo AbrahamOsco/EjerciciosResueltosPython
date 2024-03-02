0c73eeca36cff512a8ebfe4fefe46add4ef3d79b
Abraham Osco <aosco@fi.uba.ar>
2024-03-02 10:20:47 -0300

    subo la clase 1 e la cursada 3

---
+++ Cursada3/clase1.py
@@ -0,0 +1,11 @@
+print("Hola mundo")
+edad = 20
+saldo = -100
+nombre = "Antony"
+haceCalor = True
+haceFRio = False
+tengoMartillo = True
+tengoALicate = False
+resultado = (haceCalor and haceFRio) and (tengoMartillo and not tengoALicate)
+print(resultado) # False
+
